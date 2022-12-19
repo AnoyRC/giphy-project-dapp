d31596d54e8fdabec382ee1f30610e73a29449b7
Anoy Roy Chowdhury <38689344+AnoyRC@users.noreply.github.com>
2022-12-19 11:39:52 +0000

    Completed connecting solana program to the frontend

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import reactRefresh from '@vitejs/plugin-react-refresh'
-
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import { defineConfig } from "vite";
 /**
  * https://vitejs.dev/config/
  * @type { import('vite').UserConfig }
  */
-export default {
+export default defineConfig({
+  define: {
+    global: {},
+    process: {
+      'env': {}
+    }
+  },
   plugins: [reactRefresh()],
   server: {
     host: '0.0.0.0',
@@ -12,4 +18,4 @@ export default {
       port: 443,
     }
   }
-}
+})
